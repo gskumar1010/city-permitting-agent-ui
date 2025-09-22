ea8f53fc8e5582d2cea41b3e4289df87de9a03da
Lee Roland <glroland@hotmail.com>
2025-09-22 14:29:33 -0400

    bug fixes and config for using lls operator

--- chatbot/src/ai_gateway.py
+++ chatbot/src/ai_gateway.py
@@ -120,15 +120,16 @@ class AIGateway:
         # Retrieve the vector database for the mechanic application
         logger.info("Searching for the mechanic vector database.  DB_NAME=%s", self.MECHANIC_VECTOR_DB_NAME)
         mechanic_vdb = None
-        for vector_db in self.llama_stack_client.vector_dbs.list():
+        vector_db_list = self.llama_stack_client.vector_dbs.list()
+        for vector_db in vector_db_list:
             logger.debug("Vector DB vector_db_name = %s", vector_db.vector_db_name)
-            if vector_db.vector_db_name == self.MECHANIC_VECTOR_DB_NAME:
+            if vector_db.identifier == self.MECHANIC_VECTOR_DB_NAME:
                 logger.debug("VDB Match Found!")
                 mechanic_vdb = vector_db
                 break
         if mechanic_vdb is None:
-            msg = "No matching Vector Database for the Mechanic applicaiton could be found."
-            logger.error(msg)
+            msg = "No matching Vector Database for the Mechanic application could be found."
+            logger.error(msg + " DBs=%s", vector_db_list)
             raise ValueError(msg)
 
         # Query documents
